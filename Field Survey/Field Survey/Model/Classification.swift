edad4ebdd454433ed4195fd94dda9f1085d619aa
Ian Anderson <ilam56@mail.missouri.edu>
2019-11-22 13:20:16 -0600

    testing to send to TA

---
+++ Field Survey/Field Survey/Model/Classification.swift
@@ -0,0 +1,20 @@
+//
+//  Classification.swift
+//  Field Survey
+//
+//  Created by Ian Anderson on 11/22/19.
+//  Copyright © 2019 Ian Anderson. All rights reserved.
+//
+
+import UIKit
+
+enum Classification: String {
+    case amphibian
+    case bird
+    case insect
+    case plant
+    case mammal
+    case reptile
+    case fish
+    
+}
